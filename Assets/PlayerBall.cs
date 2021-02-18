90b7ea779ccadbcb27c1b15719b5cbbaf3315f83
GOOD315 <dimababoshin@mail.ru>
2021-02-18 19:23:23 +0300

    дз №1
    дз №1

---
+++ Assets/PlayerBall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollingBall
+{
+    public class PlayerBall : MonoBehaviour
+    {
+        public float Speed = 3.0f;
+        private Rigidbody _rigidbody;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        protected void Move()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+
+            _rigidbody.AddForce(movement*Speed);
+            //_rigidbody.velocity = movement * Speed;
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+        
+    }
+    
+}
